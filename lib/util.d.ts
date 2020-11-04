be6f9f0178170a4d0eb43751492f3ed0cf3f8eda
WKHAllen <wkhallen@gmail.com>
2020-11-04 14:49:09 -0600

    Implement wait groups

--- lib/util.d.ts
+++ lib/util.d.ts
@@ -1,2 +1,10 @@
 export declare function decToAscii(dec: number): string;
 export declare function asciiToDec(ascii: string): number;
+export declare function wait(ms: number): Promise<void>;
+export declare class WaitGroup {
+    private total;
+    add(numToAdd?: number): void;
+    done(numDone?: number): void;
+    wait(waitMS?: number): Promise<void>;
+    getTotal(): number;
+}
--- src/util.ts
+++ src/util.ts
@@ -17,3 +17,31 @@ export function asciiToDec(ascii: string): number {
 	}
 	return dec;
 }
+
+export async function wait(ms: number): Promise<void> {
+	return new Promise(resolve => {
+		setTimeout(resolve, ms);
+	});
+}
+
+export class WaitGroup {
+	private total: number = 0;
+
+	public add(numToAdd: number = 1): void {
+		this.total += numToAdd;
+	}
+
+	public done(numDone: number = 1): void {
+		this.total -= numDone;
+	}
+
+	public async wait(waitMS: number = 10): Promise<void> {
+		while (this.total > 0) {
+			await wait(waitMS);
+		}
+	}
+
+	public getTotal(): number {
+		return this.total;
+	}
+}
